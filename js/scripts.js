971938fe2405e03436a9cbec726eb82d04292c50
khonqc07 <khonqc07@gmail.com>
2020-02-29 07:20:06 -0800

    Feb 29 2020

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function(modal){
+    $("#reserveButton").click(function(modal){
+        $("#reserveModal").modal("show");
+    })
+    $("#loginButton").click(function(modal){
+        $("#loginModal").modal("show");
+    })
+})
